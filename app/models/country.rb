a8a2513f336f5df6e139ca8c2ec07886334b6be7
Rudy Seidinger <rudyseidinger@gmail.com>
2014-11-05 08:51:49 +0000

    Implement validation on Country to avoid associations with non-root TargetGroup

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,13 @@
 class Country < ActiveRecord::Base
   belongs_to :panel_provider
+  has_many :locations
   has_and_belongs_to_many :location_groups
-  has_and_belongs_to_many :target_groups
+  has_and_belongs_to_many :target_groups, before_add: :check_target_group_is_root
+
+  private
+  def check_target_group_is_root(target_group)
+    unless target_group.is_root_node?
+      raise "Impossible to add this target group. Only root nodes are allowed!"
+    end
+  end
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -3,7 +3,18 @@ require 'rails_helper'
 RSpec.describe Country, :type => :model do
   context "relationships" do
     it {should belong_to :panel_provider}
+    it {should have_many :locations}
     it {should have_and_belong_to_many :location_groups}
     it {should have_and_belong_to_many :target_groups}
+
+    context "target groups association" do
+      let(:country){FactoryGirl.build(:country)}
+      let(:parent_target_group){FactoryGirl.build(:target_group)}
+      let(:child_target_group){FactoryGirl.build(:target_group, parent_target_group: parent_target_group)}
+      
+      it "should only accept root nodes for this association" do
+        expect{country.target_groups << child_target_group}.to raise_error RuntimeError
+      end
+    end
   end
 end
