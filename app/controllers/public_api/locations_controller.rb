9defd9e3f3983efe5219c9536574937ce860e740
Rudy Seidinger <rudyseidinger@gmail.com>
2014-11-05 18:28:42 +0000

    Fix response codes when returning data from CountryLocationService

--- app/controllers/public_api/locations_controller.rb
+++ app/controllers/public_api/locations_controller.rb
@@ -5,7 +5,11 @@ module PublicApi
     def by_country
       country_code = params[:country_code]
       locations = CountryLocationsService.new(country_code).get_locations_through_provider
-      respond_with(locations.to_json)
+      if locations
+        respond_with(locations, status: 200)
+      else
+        respond_with(nil, status: :not_found)
+      end
     end
   end
 end
