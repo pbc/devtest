059c84cecf327e8b42b2430ffa8260c0f7afd04e
Rudy Seidinger <rudyseidinger@gmail.com>
2014-11-06 08:40:25 +0000

    Fix calculators base class initialization

--- app/price_calculation/calculators/base_price_calculator.rb
+++ app/price_calculation/calculators/base_price_calculator.rb
@@ -1,5 +1,8 @@
 module Calculators
   class BasePriceCalculator
+    def initialize(panel_provider)
+      @panel_provider = panel_provider
+    end
     def calculate_price
       raise NotImplementedError, "This method must be overrided in subclasses!"
     end
